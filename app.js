8a36cf67f61cf7ad683210ef84fdd7333a5eab5c
redshirtfull456 <nishanphuyal111@gmail.com>
2023-06-03 11:21:35 +0545

    initialized

---
+++ app.js
@@ -0,0 +1,25 @@
+require('module-alias/register');
+const express = require('express');
+const path = require('path');
+const routes = require('./routes/index');
+const ejsLayouts = require('express-ejs-layouts');
+const dayjs = require('dayjs');
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, "src/views"));
+app.use(express.json());
+app.use(express.urlencoded({extended : false}));
+app.use(express.static(path.join(__dirname, "/public")));
+app.use(ejsLayouts);
+app.set("layout extractScripts", true);
+app.locals.dayjs = dayjs;
+
+
+app.use('/', routes);
+
+
+const port = 3000;
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+})
---
+++ migrations/20230603020922-postgres.js
@@ -0,0 +1,38 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    queryInterface.sequelize.query("drop table if exists users");
+    await queryInterface.createTable("users", {
+      id: {
+        type: Sequelize.BIGINT,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      address: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        defaultValue: "Not-Set",
+      },
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+  },
+};
